b2651e9d12db6b411418643d91929cf3eefbea32
e <32040029+zhou7102@users.noreply.github.com>
2019-03-08 17:32:43 -0500

    add basic library and distance function

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,5 +1,26 @@
-def KNN ():
-    #init
-    
-def helpFunc():
-    #
+import numpy as np
+import pandas as pd
+import math
+import operator
+from __future__ import division
+
+#This is the Euclidean function to calculate distance
+def Euclidean(x1, y1, lenght):
+    distance = 0
+    for i in range(lenght):
+        distance += np.square(x1[i]-y1[i])
+    return np.sqrt(distance)
+
+#This is the Manhattan function to calculate distance    
+def Manhattan(x1, y1, lenght):
+    distance = 0
+    for i in range(lenght):
+        distance += abs(x1[i]-y1[i])
+    return distance
+
+#This is the Minkowski function to calculate distance    
+def Minkowski(x1, y1, lenght, p):
+    distance = 0
+    for i in range(lenght):
+        distance += abs(x1[i]-y1[i])**p
+    return distance**(1/p)
