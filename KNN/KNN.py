61a606b5d27404d8af300c45b6564322b1a72406
e <32040029+zhou7102@users.noreply.github.com>
2019-03-08 17:34:41 -0500

    add comment

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -18,7 +18,8 @@ def Manhattan(x1, y1, lenght):
         distance += abs(x1[i]-y1[i])
     return distance
 
-#This is the Minkowski function to calculate distance    
+#This is the Minkowski function to calculate distance
+#Not understand this function, please see https://en.wikipedia.org/wiki/Minkowski_distance    
 def Minkowski(x1, y1, lenght, p):
     distance = 0
     for i in range(lenght):
